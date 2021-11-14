64881fb5a56561f5efc919a42d06e050d0c8139a
harveytriana <harveytriana@hotmail.com>
2021-11-14 14:55:39 -0500

    First Push

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Welcome net6 
+ */
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Console Application Esperiments\n");
+
+RustTest.Run();
+
+Console.WriteLine("\nReady");
+
+#region Rust Test
+/// <summary>
+/// How to call a Rust's code function from C# 
+/// </summary>
+class RustTest
+{
+    const string RUSTLIB = @"..\..\..\..\rstlib\target\release\rust_library.dll";
+
+    [DllImport(RUSTLIB)] static extern void greeting();
+
+    [DllImport(RUSTLIB)] static extern float hypotenuse(float x, float y);
+
+    [DllImport(RUSTLIB)] static extern int counter();
+
+    public static void Run()
+    {
+        Console.WriteLine("\nCalling Rust Experiments");
+        Console.WriteLine("greetings subrutine:"); 
+        greeting(); // rust writes in console
+
+        // rust executes a math formula
+        float x = 9, y = 11, h = hypotenuse(x, y);
+
+        Console.WriteLine("\nHypotenuse({0}, {1}) = {2}", x, y, h);
+
+        Console.WriteLine("\nCOUNTER");
+        Console.WriteLine("{0}", counter());
+        Console.WriteLine("{0}", counter());
+        Console.WriteLine("{0}", counter());
+    }
+}
+#endregion
---
+++ rstlib/src/lib.rs
@@ -0,0 +1,34 @@
+/*
+----------------------------------------------------
+Interoperability exercise
+COMPILE
+.dll
+cargo build --release
+
+.a (WebAssembly)
+cargo build --target wasm32-unknown-emscripten --release
+----------------------------------------------------
+*/
+// basic test
+#[no_mangle]
+pub extern "C" fn greeting() {
+    println!("Hello World!");
+}
+
+// math function
+#[no_mangle]
+pub fn hypotenuse(x: f32, y: f32) -> f32 {
+    let num = x.powi(2) + y.powi(2);
+    num.powf(0.5)
+}
+
+// shared variable
+static mut COUNTER: i32 = 0;
+
+#[no_mangle]
+pub extern "C" fn counter() -> i32 {
+    unsafe {
+        COUNTER += 1;
+        return COUNTER;
+    }
+}
