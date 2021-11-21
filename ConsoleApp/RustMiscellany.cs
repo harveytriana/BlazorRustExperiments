bd5fd122f6a196866498790df3e91fe8ba4cd7a0
harveytriana <harveytriana@hotmail.com>
2021-11-21 17:14:35 -0500

    RustMiscellanyPage

--- ConsoleApp/RustMiscellany.cs
+++ ConsoleApp/RustMiscellany.cs
@@ -19,8 +19,6 @@ class RustMiscellany
 
     [DllImport(RUSTLIB)] static extern IntPtr describe_person(int age);
 
-    [DllImport(RUSTLIB)] static extern IntPtr get_user(int user_id);
-
     public static void Run()
     {
         Console.ForegroundColor = ConsoleColor.DarkYellow;
@@ -31,9 +29,9 @@ class RustMiscellany
         Console.WriteLine("\nCALLING MATH FUNCTION");
         float x = 9, y = 11, h = hypotenuse(x, y);
 
-        Console.WriteLine("Hypotenuse({0}, {1}) = {2}", x, y, h);
+        Console.WriteLine("From Rust Library, Hypotenuse({0}, {1}) = {2}", x, y, h);
 
-        Console.WriteLine("\nCOUNTER FROM CLOSURE", "green");
+        Console.WriteLine("\nCOUNTER FROM CLOSURE");
         Console.WriteLine("Counter: {0}", counter());
         Console.WriteLine("Counter: {0}", counter());
         Console.WriteLine("Counter: {0}", counter());
@@ -44,7 +42,7 @@ class RustMiscellany
 
         Console.WriteLine("Send a string to Rust (with extended characters)");
         Console.ForegroundColor = ConsoleColor.DarkYellow;
-        print_string("« Sin música, la vida sería un error »");
+        print_string("■ Hello from Blazor");
         Console.ForegroundColor = ConsoleColor.Gray;
 
         // ** Another approach, but moore complexity
@@ -57,16 +55,13 @@ class RustMiscellany
         var text = Marshal.PtrToStringUTF8(ptr);
 
         Console.WriteLine("String Pointer : {0}", ptr);
-        Console.WriteLine("Dereference    : {0}", text);
+        Console.WriteLine("Dereferenced   : {0}", text);
 
-        Console.WriteLine("\nString function: ");
+        Console.WriteLine("\nString function:");
         int age = 18;
         ptr = describe_person(age);
-        text = ptr.TextFromPointer(); // with extension
-        // OR
-        // text = Marshal.PtrToStringUTF8(ptr);
          
-        Console.WriteLine("describe_person(age: {0}) : {1}",age, text);
+        Console.WriteLine("describe_person(age: {0}) : {1}",age, ptr.TextFromPointer());
 
     }
 }
