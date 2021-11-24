212e13ea5140eb6e393b8c10ab41e83f980f0274
harveytriana <harveytriana@hotmail.com>
2021-11-24 10:01:02 -0500

    Callback Page

---
+++ BlazorRustExperiments/BlazorRustExperiments/CallbackExperiment.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BlazorRustExperiments;
+
+public unsafe class CallbackExperiment
+{
+    // echo to consumer
+    public delegate Task EchoHandler(string? message);
+    public static EchoHandler Echo;
+
+    static readonly delegate* unmanaged<int, void> OnRaiseNumberPointer = &OnRaiseNumber;
+
+    public static void Run()
+    {
+        Console.WriteLine("\nRunning C++");
+
+        // call a C++ method
+        UnmanagedPrompt((IntPtr)OnRaiseNumberPointer);
+    }
+
+    [UnmanagedCallersOnly]
+    private static void OnRaiseNumber(int number)
+    {
+        Echo?.Invoke($"Arrives external number: {number}");
+    }
+
+    // extern ---------------------------------------------------------------
+    [DllImport(Global.RLIB)] static extern void UnmanagedPrompt(IntPtr notify);
+    // theory
+    // [DllImport(CLIB)] static extern void UnmanagedPrompt(delegate *unmanaged<int,void> cppCallback);
+}
